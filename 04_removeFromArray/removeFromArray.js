f2b7a2008afe8642b234dc5fad0199381c3efaaa
ryantohhr <ryantohhongrui@hotmail.com>
2024-10-23 15:50:27 +0800

    Completed 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
+    for (let i = 0; i < toRemove.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (toRemove[i] === array[j]) {
+                array.splice(j, 1);
+                j--;
+            };
+        };
+    };
+    return array;
 };
 
 // Do not edit below this line
