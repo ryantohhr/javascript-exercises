a03fdd6cf196779a42b7e3067ff040391f06fde1
ryantohhr <ryantohhongrui@hotmail.com>
2024-10-23 16:12:41 +0800

    Completed 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,27 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    num1Type = typeof num1;
+    num2Type = typeof num2;
+    num1 = Number(num1);
+    num2 = Number(num2);
+    if (!(Number.isInteger(num1)) || !(Number.isInteger(num2))) {
+        return "ERROR";
+    }
+    else if (num1Type !== "number" || num2Type !== "number") {
+        return "ERROR";
+    }
+    else if (num1 > num2) {
+        let tmp = num1;
+        num1 = num2;
+        num2 = tmp;
+    }
+    else if ((num1 < 0) || (num2 < 0)) {
+        return "ERROR";
+    }
+    for (let i = num1; i < num2 + 1; i++) {
+        sum += i;
+    };
+    return sum;
 
 };
 
