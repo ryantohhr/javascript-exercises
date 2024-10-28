79a702e32e036e24ea7229f4d0e3fa2eae393121
ryantohhr <ryantohhongrui@hotmail.com>
2024-10-28 17:09:12 +0800

    Completed 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const year = new Date().getFullYear();
+    return people.reduce((oldest, current) => {
+        if (current.yearOfDeath) {
+            currentAge = current.yearOfDeath - current.yearOfBirth;
+        }
+        else {
+            currentAge = year - current.yearOfBirth;
+        }
+        if (oldest.yearOfDeath) {
+            oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+        }
+        else {
+            oldestAge = year - oldest.yearOfBirth;
+        }
+        if (oldestAge > currentAge) {
+            return oldest;
+        }
+        else {
+            return current;
+        }
+    }, {})
 };
 
 // Do not edit below this line
