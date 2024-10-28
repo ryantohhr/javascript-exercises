7661d9bcaa31e33a16a875769ae3a1cca13f3578
ryantohhr <ryantohhongrui@hotmail.com>
2024-10-28 16:40:27 +0800

    Completed 08 and 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // Removing space and punctuation
+    string = string.replace(/\ /g, "").replace(/\./g, "").replace(/\!/g, "").replace(/\,/g, "");
 
+    // Convert string to lower case
+    string = string.toLowerCase();
+
+    // Check palindrome
+    let n = Math.floor(string.length / 2);
+    let firstHalf = string.slice(0, n);
+    let firstHalfReversed = firstHalf.split("").reverse().join("");
+    let secondHalf = string.slice(string.length - n);
+    if (firstHalfReversed === secondHalf) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
