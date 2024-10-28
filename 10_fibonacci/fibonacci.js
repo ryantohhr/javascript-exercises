c66189f57eabd7d75c523efc8b3333ec0ad1ab2c
ryantohhr <ryantohhongrui@hotmail.com>
2024-10-28 16:53:29 +0800

    Completed 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(member) {
+    member = Number(member);
+    if (member < 0) {
+        return "OOPS";
+    }
+    else if (member === 1) {
+        return 1;
+    }
+    else if (member === 0) {
+        return 0;
+    }
+    else {
+        return fibonacci(member - 1) + fibonacci(member - 2);
+    }
 };
 
 // Do not edit below this line
